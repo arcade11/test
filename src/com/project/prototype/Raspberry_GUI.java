483a37e9640863b236ed8063b12be411b888f75f
arcade11 <32836201+arcade11@users.noreply.github.com>
2018-03-15 15:27:58 +0100

    Textfield

--- src/com/project/prototype/Raspberry_GUI.java
+++ src/com/project/prototype/Raspberry_GUI.java
@@ -6,10 +6,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
 
 public class Raspberry_GUI extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Create the frame.
@@ -18,11 +20,16 @@ public class Raspberry_GUI extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//setUndecorated(true);
 		
-		setBounds(100, 100, 295, 225);
+		setBounds(100, 100, 581, 384);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(25, 23, 263, 171);
+		contentPane.add(textField);
+		textField.setColumns(10);
 		
 		setVisible(true);
 	}
