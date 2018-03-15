f49e593dc14dcda8608f202c66844e71cdfa627c
arcade11 <32836201+arcade11@users.noreply.github.com>
2018-03-15 15:19:32 +0100

    window size

--- src/com/project/prototype/Raspberry_GUI.java
+++ src/com/project/prototype/Raspberry_GUI.java
@@ -18,7 +18,7 @@ public class Raspberry_GUI extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//setUndecorated(true);
 		
-		setBounds(100, 100, width, height);
+		setBounds(100, 100, 295, 225);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
